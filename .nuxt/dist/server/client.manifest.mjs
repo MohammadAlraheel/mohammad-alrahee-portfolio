4878d015a8c2865e10621f9e98ce98ebcbf3cfb8
Sharif Alalem <sharef.alalem101@gmail.com>
2025-08-24 23:28:09 +0200

    delete unused

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/sharifalalem/Desktop/mohammad-alrahee-portfolio/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- components/WorksSection.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <p>test</p>
-</template>
-
-<script setup lang="ts">
-
-</script>
